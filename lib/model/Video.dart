d7671069e57728cfe7283a7470ca762a4ed8b5cb
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-23 17:24:27 -0300

    recuperando dados via api

---
+++ lib/model/Video.dart
@@ -0,0 +1,9 @@
+class Video {
+  String id;
+  String titulo;
+  String descricao;
+  String imagem;
+  String canal;
+
+  Video(this.id, this.canal, this.descricao, this.imagem, this.titulo);
+}
--- lib/telas/Inicio.dart
+++ lib/telas/Inicio.dart
@@ -1,17 +1,19 @@
+import 'package:app_youtube/Api.dart';
 import 'package:flutter/material.dart';
 
 class Inicio extends StatefulWidget {
-  const Inicio({super.key});
-
   @override
-  State<Inicio> createState() => _InicioState();
+  _InicioState createState() => _InicioState();
 }
 
 class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
+    Api api = Api();
+    api.pesquisar("");
+
     return Container(
-      child: Center(child: Text("Inicio", style: TextStyle(fontSize: 25))),
+      child: Center(child: Text("Início", style: TextStyle(fontSize: 25))),
     );
   }
 }
