efb7c0bd827c54a3025a95798e96eaf30e37c9b5
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-24 14:42:49 -0300

    resolvendo a api e convertendo dados json em um mapa e os dados do mapa em um objeto da classe video

--- lib/Api.dart
+++ lib/Api.dart
@@ -1,30 +1,41 @@
+import 'package:app_youtube/model/Video.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 const CHAVE_YOUTUBE_API = "AIzaSyDLPkI16FU8FrC5_SOQldS30Nvof2B5-eU";
 const ID_CANAL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
-const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+const URL_BASE = "www.googleapis.com";
 
 class Api {
   pesquisar(String pesquisa) async {
-    http.Response response = await http.get(
-      Uri.parse(
-        "$URL_BASE/search"
-        "?part=snippet"
-        "&type=video"
-        "&maxResults=20"
-        "&order=date"
-        "&key=$CHAVE_YOUTUBE_API"
-        "&channelId=$ID_CANAL"
-        "&q=$pesquisa",
-      ),
-    );
+    final uri = Uri.https(URL_BASE, "/youtube/v3/search", {
+      "part": "snippet",
+      "type": "video",
+      "maxResults": "20",
+      "order": "date",
+      "key": CHAVE_YOUTUBE_API,
+      "channelId": ID_CANAL,
+      "q": pesquisa,
+    });
 
-    if (response.statusCode == 200) {
-      Map<String, dynamic> dadosJson = json.decode(response.body);
-      print(
-        "resultado: " + dadosJson["items"][2]["snippet"]["title"].toString(),
-      );
-    } else {}
+    http.Response response = await http.get(uri);
+
+    Map<String, dynamic> dadosJson = json.decode(response.body);
+
+    List<Video> videos =
+        dadosJson["items"].map<Video>((map) {
+          return Video.fromJson(map);
+          //return Video.converterJson(map);
+        }).toList();
+
+    for (var video in videos) {
+      print("resultado " + video.titulo);
+    }
+
+    /*for (var video in dadosJson["items"]) {
+      print("resultado" + video.toString());
+    }*/
+
+    //print("resultado: " + dadosJson["items"].toString());
   }
 }
--- lib/model/Video.dart
+++ lib/model/Video.dart
@@ -5,5 +5,15 @@ class Video {
   String imagem;
   String canal;
 
-  Video(this.id, this.canal, this.descricao, this.imagem, this.titulo);
+  Video(this.id, this.titulo, this.descricao, this.imagem, this.canal);
+
+  factory Video.fromJson(Map<String, dynamic> json) {
+    return Video(
+      json["id"]["videoId"],
+      json["snippet"]["title"],
+      json["snippet"]["description"], // Corrigido
+      json["snippet"]["thumbnails"]["high"]["url"],
+      json["snippet"]["channelTitle"], // Corrigido: era "channelId", mas deveria ser "channelTitle"
+    );
+  }
 }
